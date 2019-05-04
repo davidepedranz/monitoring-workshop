df6620436f82d2bb4ff4edc41117e25adb7b9e10
Davide Pedranz <dp@bendingspoons.com>
2019-05-04 09:45:26 +0200

    [backend] In memory database

---
+++ backend/app/main.py
@@ -0,0 +1,34 @@
+import os
+from typing import NoReturn
+
+from flask import Flask, Response
+
+from app.apis.todo import make_todos_blueprint
+from app.repository.base import Repository
+from app.repository.memory import InMemoryRepository
+
+
+def main() -> None:
+    repository = InMemoryRepository()
+    run_flask_app(repository)
+
+
+def run_flask_app(repository: Repository) -> NoReturn:
+    host = os.environ.get("FLASK_HOST", "0.0.0.0")
+    port = os.environ.get("FLASK_PORT", 5000)
+
+    app = Flask(__name__)
+    app.after_request(_cors_support)
+    app.register_blueprint(make_todos_blueprint(repository))
+    app.run(host=host, port=port)
+
+
+def _cors_support(response: Response) -> Response:
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Methods", "*")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    return response
+
+
+if __name__ == "__main__":
+    main()
